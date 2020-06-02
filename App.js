bb994ed57c272ec9d0003e2a49da628784928350
Christopher David <chris@arcade.city>
2020-06-02 13:50:08 -0500

    FCL config challenge.handshake

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import * as fcl from '@onflow/fcl'
 
+fcl.config().put('challenge.handshake', '<http://localhost:8701/flow/authenticate>')
+
 export default function App() {
     return (
         <View style={styles.container}>
